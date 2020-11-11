1f4b5b1eccac3728bd778742d5aeba0a41ba3c32
dotslash21 <arunangshubsws@gmail.com>
2020-11-11 15:44:46 +0530

    [SOLVED] [ARRAY] [V. IMP] Find Largest sum contiguous subarray

---
+++ array/largest_sum_contiguous_subarray.cpp
@@ -0,0 +1,55 @@
+/**
+ * @file largest_sum_contiguous_subarray.cpp
+ * @author Arunangshu Biswas (arunangshubsws@gmail.com)
+ * @brief https://practice.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1
+ * @version 0.1
+ * @date 2020-11-11
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to find subarray with maximum sum
+// arr: input array
+// n: size of array
+int maxSubarraySum(int arr[], int n)
+{
+    int max_so_far = 0;
+    int max_ending_here = 0;
+
+    for (int i = 0; i < n; i++) {
+        max_ending_here += arr[i];
+
+        if (max_so_far < max_ending_here) {
+            max_so_far = max_ending_here;
+        }
+
+        if (max_ending_here < 0) {
+            max_ending_here = 0;
+        }
+    }
+
+    return max_so_far;
+}
+
+int main()
+{
+    int t, n;
+
+    cin >> t;   //input testcases
+    while (t--) //while testcases exist
+    {
+
+        cin >> n; //input size of array
+
+        int a[n];
+
+        for (int i = 0; i < n; i++)
+            cin >> a[i]; //inputting elements of array
+
+        cout << maxSubarraySum(a, n) << endl;
+    }
+}
