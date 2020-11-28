af133fd09b14457e8b72d26f63a817069e35ad07
dotslash21 <arunangshubsws@gmail.com>
2020-11-29 01:03:08 +0530

    [OPTIMIZED] [ARRAY] Minimum no. of Jumps to reach end of an array

--- array/minimum_number_of_jumps.cpp
+++ array/minimum_number_of_jumps.cpp
@@ -9,16 +9,19 @@
  * 
  */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 // Function to return minimum number of jumps to end of array
-int minJumps(int arr[], int n){
+int minJumps(int arr[], int n)
+{
     int dp[n];
     dp[n - 1] = 0;
 
-    for (int i = n - 2; i > -1; i--) {
-        if (arr[i] == 0) {
+    for (int i = n - 2; i > -1; i--)
+    {
+        if (arr[i] == 0)
+        {
             dp[i] = -1;
             continue;
         }
@@ -26,8 +29,10 @@ int minJumps(int arr[], int n){
         int start = i + 1;
         int end = min(n, start + arr[i]);
         int minJumps = INT_MAX;
-        for (int j = start; j < end; j++) {
-            if (dp[j] == -1) continue;
+        for (int j = start; j < end; j++)
+        {
+            if (dp[j] == -1)
+                continue;
             minJumps = min(minJumps, 1 + dp[j]);
         }
         dp[i] = (minJumps == INT_MAX) ? -1 : minJumps;
@@ -36,19 +41,56 @@ int minJumps(int arr[], int n){
     return dp[0] > n ? -1 : dp[0];
 }
 
+int minJumpsOptimized(int arr[], int n)
+{
+    if (n <= 1)
+        return 0;
+
+    if (arr[0] == 0)
+        return -1;
+
+    int maxReachable, stepsPossible, numJumps;
+
+    maxReachable = arr[0];
+    stepsPossible = arr[0];
+    numJumps = 1;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (i == n - 1)
+            return numJumps;
+
+        maxReachable = max(maxReachable, i + arr[i]);
+
+        stepsPossible--;
+
+        if (stepsPossible == 0)
+        {
+            numJumps++;
+
+            if (i >= maxReachable)
+                return -1;
+
+            stepsPossible = maxReachable - i;
+        }
+    }
+
+    return -1;
+}
+
 int main()
 {
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
-        int n,i,j;
-        cin>>n;
+        int n, i, j;
+        cin >> n;
         int arr[n];
-        for(int i=0; i<n; i++)
-            cin>>arr[i];
-            
-        cout<<minJumps(arr, n)<<endl;
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+
+        cout << minJumps(arr, n) << endl;
     }
     return 0;
 }
